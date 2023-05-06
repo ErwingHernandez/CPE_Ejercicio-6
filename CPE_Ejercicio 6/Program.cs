81f53a6801513afe35b40d5e17a740a50c76f50f
Erwing Hernandez <birc89@hotmail.com>
2023-05-05 21:47:01 -0600

    Agregar archivos de proyecto.

---
+++ CPE_Ejercicio 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE_Ejercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rec1 = new Rectangulo("Rectangulo 1", 10, 20);
+            Circulo cir1 = new Circulo("Circulo 1", 20);
+
+            Console.WriteLine(rec1.ToString());
+            Console.WriteLine(cir1.ToString());
+            Console.ReadKey();
+        }
+    }
+}
